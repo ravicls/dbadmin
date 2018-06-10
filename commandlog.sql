ffa6e5f4017189c1509f50b40c56560c50fcbfad
ravicls <40145709+ravicls@users.noreply.github.com>
2018-06-10 05:58:09 -1000

    Add files via upload

---
+++ commandlog.sql
@@ -0,0 +1,45 @@
+USE [dbadmin]
+GO
+
+/****** Object:  Table [dbo].[CommandLog]    Script Date: 6/10/2018 10:57:52 AM ******/
+DROP TABLE [dbo].[CommandLog]
+GO
+
+/****** Object:  Table [dbo].[CommandLog]    Script Date: 6/10/2018 10:57:52 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[CommandLog](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[DatabaseName] [sysname] NULL,
+	[SchemaName] [sysname] NULL,
+	[ObjectName] [sysname] NULL,
+	[ObjectType] [char](2) NULL,
+	[IndexName] [sysname] NULL,
+	[IndexType] [tinyint] NULL,
+	[StatisticsName] [sysname] NULL,
+	[PartitionNumber] [int] NULL,
+	[ExtendedInfo] [xml] NULL,
+	[Command] [nvarchar](max) NOT NULL,
+	[CommandType] [nvarchar](60) NOT NULL,
+	[StartTime] [datetime] NOT NULL,
+	[EndTime] [datetime] NULL,
+	[ErrorNumber] [int] NULL,
+	[ErrorMessage] [nvarchar](max) NULL,
+ CONSTRAINT [PK_CommandLog] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
